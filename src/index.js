bd17a99a8a795244a74006e5ae325482cab68117
Alejo Fernandez <alejo.fernandez@gmail.com>
2016-05-17 14:09:56 -0300

    Change react-hot in favor of custom hot reload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import createApp from './App';
 import ReactDom from 'react-dom';
 import React from 'react';
 import { Stuffy } from '../lib';
+import RedBox from 'redbox-react';
 
 module.exports.create = function create($element) {
   const stuffy = new Stuffy();
-  const App = createApp(React, stuffy);
 
-  return ReactDom.render(
-    React.createElement(App),
-    $element
-  );
+  function hotReload() {
+    try {
+      render();
+    } catch (error) {
+      renderError(error);
+    }
+  }
+
+  function render() {
+    const createApp = require('./App').default;
+    const App = createApp(React, stuffy);
+    ReactDom.render(<App />, $element);
+  }
+
+  function renderError(error) {
+    ReactDom.render(<RedBox error={error} />, $element);
+  }
+
+  function setupHotModuleReload() {
+    return module.hot && module.hot.accept('./App', () => setTimeout(hotReload));
+  }
+
+  setupHotModuleReload();
+
+  return hotReload();
 };
